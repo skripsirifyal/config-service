f219da1443e4a76c94e4e5985ccdd05dfeab1c5f
Moh Rifyal Azmi <skripsirifyal@gmail.com>
2022-08-05 01:59:00 +0700

    Initial commit

---
+++ src/test/java/id/ac/upiyai/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.upiyai.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
